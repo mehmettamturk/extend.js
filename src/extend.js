eadda9fec8572d52c1d443694752a4b981fe3036
mehmettamturk <mehmettamturk@hotmail.com.tr>
2012-07-25 17:05:17 +0300

    Added contains and wordCount methods for String.

--- src/extend.js
+++ src/extend.js
@@ -25,3 +25,26 @@ Number.prototype.greaterThan = function(num) {
 Number.prototype.smallerThan = function(num) {
     return this < num;
 }
+
+/**
+ * Is array contains requested char or string.
+ * @param {string | number} char is requested string. 
+ * @return {Number}
+ */
+String.prototype.contains = function(char) {
+    return this.indexOf(char) != -1;
+};
+
+/**
+ * Returns count of requested param. 
+ *
+ * @param {string | number} char is requested string. 
+ * @param {Boolean=} cs is case sensitive. 
+ * @return {Number} 
+ */
+String.prototype.wordCount = function(char, cs) {
+    if (cs) 
+        return this.toLowerCase().split(char.toLowerCase()).length - 1;
+    else
+        return this.split(char).length - 1;
+};
